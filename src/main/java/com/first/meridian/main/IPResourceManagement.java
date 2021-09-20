85eb5047a261bbc0a0e81c634ef9adb8f90dc019
praveen <praveenkuruva@gmail.com>
2021-09-20 21:57:40 +0530

    JIRA-ID2-Added Main Class

---
+++ src/main/java/com/first/meridian/main/IPResourceManagement.java
@@ -0,0 +1,11 @@
+package com.first.meridian.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IPResourceManagement {
+    public static void main(String[] args) {
+        SpringApplication.run(IPResourceManagement.class, args);
+    }
+}
