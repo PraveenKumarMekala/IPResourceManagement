fe86d17465b707e371b5f2f8700697e6dcaa2318
praveen <praveenkuruva@gmail.com>
2021-09-22 21:57:17 +0530

    JIRA-ID8- Added h2 dependency

--- src/main/java/com/first/meridian/main/IPResourceManagement.java
+++ src/main/java/com/first/meridian/main/IPResourceManagement.java
@@ -3,12 +3,16 @@ package com.first.meridian.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackages = "com.first.meridian")
 public class IPResourceManagement {
     public static void main(String[] args) {
         SpringApplication.run(IPResourceManagement.class, args);
