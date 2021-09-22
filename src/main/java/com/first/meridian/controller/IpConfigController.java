24e7419cb57e809f0715e21f8610b294eb88dbf4
praveen <praveenkuruva@gmail.com>
2021-09-22 21:49:27 +0530

    JIRA-ID7- Added slf4J logger with logback.xml

--- src/main/java/com/first/meridian/controller/IpConfigController.java
+++ src/main/java/com/first/meridian/controller/IpConfigController.java
@@ -1,6 +1,8 @@
 package com.first.meridian.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,8 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 @RequestMapping("api/v1")
 public class IpConfigController {
+
+    private static final Logger logger = LoggerFactory.getLogger(IpConfigController.class);
 
     @PostMapping(consumes = {APPLICATION_JSON_VALUE}, produces = {APPLICATION_JSON_VALUE})
     @Operation(method = "POST", summary = "",
