e339a292c3e85106f032ba500cbf15388712f324
praveen <praveenkuruva@gmail.com>
2021-09-23 14:03:52 +0530

    JIRA-ID13- Added arg constructors

--- src/main/java/com/first/meridian/domain/IpAddress.java
+++ src/main/java/com/first/meridian/domain/IpAddress.java
@@ -1,14 +1,14 @@
 package com.first.meridian.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.Builder;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "IP_ADDRESS")
 public class IpAddress {
--- src/main/java/com/first/meridian/domain/IpPool.java
+++ src/main/java/com/first/meridian/domain/IpPool.java
@@ -10,6 +10,8 @@ import java.util.List;
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "T_IP_POOL")
 public class IpPool {
