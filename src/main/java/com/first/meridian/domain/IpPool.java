d7b8801ed26ce2255d76e17a3a07555045435cc5
praveen <praveenkuruva@gmail.com>
2021-09-23 13:53:11 +0530

    JIRA-ID12- Added embedded ID

--- src/main/java/com/first/meridian/domain/IpPool.java
+++ src/main/java/com/first/meridian/domain/IpPool.java
@@ -1,29 +1,42 @@
 package com.first.meridian.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.Builder;
+import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import java.util.List;
 
 @Getter
 @Setter
 @Builder
 @Entity
-@Table
+@Table(name = "T_IP_POOL")
 public class IpPool {
 
     @Id
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "ipPoolId")
-    private Long ipPoolId;
+    @Column(name = "description")
+    private String description;
 
-    @Column(name = "value")
-    private Long value;
+    @Column(name = "totalCapacity")
+    @Max(value = 250, message = "Max pool size can be 250")
+    private Long totalCapacity;
+
+    @Column(name = "usedCapacity")
+    private Long usedCapacity;
+
+    @Column(name = "lowerBound")
+    @Min(value = 0, message = "Minimum size should be zero")
+    private Long lowerBound;
+
+    @Column(name = "upperBound")
+    @Max(value = 250, message = "Maximum ipAddresses can be of size 250")
+    private Long upperBound;
+
+    @OneToMany(mappedBy = "id.ipPoolId", fetch = FetchType.EAGER, cascade = {CascadeType.ALL})
+    private List<IpAddress> ipAddresses;
 
 }
